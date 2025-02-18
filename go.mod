74cde0bb4f53c2f8e875d9ffc78124e9418db0e5
Markus Mahlberg <markus.mahlberg@dacoso.com>
2025-02-18 13:40:18 +0100

    style: use own context

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/alecthomas/kong v1.8.1
 	github.com/davecgh/go-spew v1.1.1
 	github.com/jstemmer/go-junit-report v1.0.0
+	github.com/pkg/errors v0.9.1
 )
 
 require (
--- main.go
+++ main.go
@@ -11,61 +11,83 @@ import (
 	"github.com/alecthomas/kong"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/jstemmer/go-junit-report/formatter"
+	"github.com/pkg/errors"
 )
 
 type printer struct {
 	Type string `arg:"" help:"Type of resource to print." name:"css|html" enum:"css,html"`
 }
 
-func (p *printer) Run(ctx *kong.Context) error {
+func (p *printer) Run(ctx context) error {
 	switch p.Type {
 	case "css":
-		fmt.Println(styles)
+		fmt.Println(defaultStyles)
 	case "html":
-		fmt.Println(templateData)
+		fmt.Println(defaultTemplate)
 	}
 	return nil
 }
 
 type generator struct {
-	JunitXML *os.File `arg:"" help:"Path to the JUnit XML file to generate a report from. use '-' for stdin."`
+	JunitXML *os.File `arg:"" help:"Path to the JUnit XML file to generate a report from. Use '-' for stdin."`
+	Template string   `short:"t" type:"existingfile" help:"Use an alternate template." placeholder:"/path/to/go.tmpl" group:"Rendering options"`
+	CSS      string   `short:"s" name:"styles" type:"filecontent" help:"Use an alternate CSS file." placeholder:"/path/to.css" group:"Rendering options"`
 }
 
-func (g *generator) Run(ctx *kong.Context) error {
+func (g *generator) Run(ctx *context) (err error) {
 	defer g.JunitXML.Close()
-	ctx.FatalIfErrorf(xml.NewDecoder(g.JunitXML).Decode(&suites))
+	tmpl := template.Must(template.New("report").Funcs(sprig.FuncMap()).Parse(ctx.defaultTemplate))
+
+	if g.Template != "" {
+		tmpl, err = template.New("report").Funcs(sprig.FuncMap()).ParseFiles(g.Template)
+		if err != nil {
+			return errors.Wrap(err, "failed to parse template")
+		}
+	}
+
+	var suites formatter.JUnitTestSuites
+	if err = xml.NewDecoder(g.JunitXML).Decode(&suites); err != nil {
+		return errors.Wrap(err, "failed to decode JUnit XML")
+	}
+
 	tmplCtx := struct {
 		Suites []formatter.JUnitTestSuite
 		CSS    template.CSS
 	}{
 		Suites: suites.Suites,
-		CSS:    template.CSS(styles),
+		CSS:    template.CSS(defaultStyles),
 	}
+
 	if cfg.Debug {
 		spew.Dump(tmplCtx)
 		spew.Dump(suites)
 	}
-
-	return tmpl.Execute(os.Stdout, tmplCtx)
+	return tmpl.ExecuteTemplate(os.Stdout, "report", tmplCtx)
 }
 
 var (
 	//go:embed style.css
-	styles string
+	defaultStyles string
 	//go:embed report.gohtml
-	templateData string
-	tmpl         = template.Must(template.New("report").Funcs(sprig.FuncMap()).Parse(templateData))
-	suites       formatter.JUnitTestSuites
-	cfg          struct {
-		Debug    bool      `short:"d" long:"debug" description:"Show debug information"`
+	defaultTemplate string
+	// suites          formatter.JUnitTestSuites
+	cfg struct {
+		Debug    bool      `short:"d" long:"debug" help:"Show debug information"`
 		Print    printer   `cmd:"print" help:"Print an embedded resource"`
 		Generate generator `cmd:"generate" help:"Generate a report from the input XML"`
 	}
 )
 
+type context struct {
+	defaultTemplate string
+	defaultStyles   string
+}
+
 func main() {
 
-	ctx := kong.Parse(&cfg)
-	err := ctx.Run()
+	ctx := kong.Parse(&cfg,
+		kong.Name("junit-report"),
+		kong.Description("Generate a HTML report from JUnit XML"))
+	err := ctx.Run(&context{defaultStyles: defaultStyles, defaultTemplate: defaultTemplate})
 	ctx.FatalIfErrorf(err)
 }
